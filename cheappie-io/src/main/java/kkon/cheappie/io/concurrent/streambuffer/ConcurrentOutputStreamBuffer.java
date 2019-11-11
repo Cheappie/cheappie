2c9a63ef68064bdb15299a842df8ec9b297b3f1c
Kamil Konior <fasterdatabear@gmail.com>
2019-11-11 22:22:55 +0100

    escape wildcard import

--- cheappie-io/src/main/java/kkon/cheappie/io/concurrent/streambuffer/ConcurrentOutputStreamBuffer.java
+++ cheappie-io/src/main/java/kkon/cheappie/io/concurrent/streambuffer/ConcurrentOutputStreamBuffer.java
@@ -1,33 +1,34 @@
 /*
  * Copyright (C) 2009 The Cheappie Authors
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 
 package kkon.cheappie.io.concurrent.streambuffer;
 
 import com.google.common.base.Preconditions;
-
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.LongAdder;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Stream;
-
 import static java.util.stream.Collectors.toCollection;
 import static org.apache.commons.lang3.exception.ExceptionUtils.rethrow;
 
@@ -51,7 +52,7 @@ public class ConcurrentOutputStreamBuffer implements Closeable {
     private final CompletionNotifier taskCompletionNotifier;
 
     private ConcurrentOutputStreamBuffer(OutputStream outputStream, int minAssumedConcurrency,
-                                         int minBytesWrittenUntilFlush, int memThrottleFactor, int cpuThrottleFactor) {
+                    int minBytesWrittenUntilFlush, int memThrottleFactor, int cpuThrottleFactor) {
         Preconditions.checkArgument(memThrottleFactor > minAssumedConcurrency,
                         "Required: throttleFactor > minAssumedConcurrency");
 
@@ -75,7 +76,7 @@ public class ConcurrentOutputStreamBuffer implements Closeable {
     }
 
     public static ConcurrentOutputStreamBuffer of(OutputStream outputStream, int minAssumedConcurrency,
-                                                  int minBytesWrittenUntilFlush, int memThrottleFactor, int cpuThrottleFactor) {
+                    int minBytesWrittenUntilFlush, int memThrottleFactor, int cpuThrottleFactor) {
         return new ConcurrentOutputStreamBuffer(outputStream, minAssumedConcurrency, minBytesWrittenUntilFlush,
                         memThrottleFactor, cpuThrottleFactor);
     }
