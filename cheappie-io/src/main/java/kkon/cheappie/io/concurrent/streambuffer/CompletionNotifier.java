7d58c72a5dba0253a4eff8a3ba9147833bcf8b17
Kamil Konior <fasterdatabear@gmail.com>
2019-11-12 01:08:07 +0100

    Reuse explicit 'this'

--- cheappie-io/src/main/java/kkon/cheappie/io/concurrent/streambuffer/CompletionNotifier.java
+++ cheappie-io/src/main/java/kkon/cheappie/io/concurrent/streambuffer/CompletionNotifier.java
@@ -1,15 +1,14 @@
 /*
  * Copyright (C) 2009 The Cheappie Authors
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 
 package kkon.cheappie.io.concurrent.streambuffer;
@@ -42,10 +41,10 @@ public final class CompletionNotifier {
     }
 
     void runAsyncWhenDone(Callable<Void> callable) {
-        CompletableFuture.runAsync(() -> invokeIgnoringExceptions(new RecurringAsyncTask(task, callable)));
+        CompletableFuture.runAsync(new RecurringAsyncTask(task, callable));
     }
 
-    private static class RecurringAsyncTask implements Callable<Void> {
+    private static class RecurringAsyncTask implements Runnable {
         private final Future<Void> task;
         private final Callable<Void> callable;
 
@@ -55,21 +54,15 @@ public final class CompletionNotifier {
         }
 
         @Override
-        public Void call() throws Exception {
-            if (task.isDone()) {
-                callable.call();
-            } else {
-                CompletableFuture.runAsync(() -> invokeIgnoringExceptions(this));
+        public void run() {
+            try {
+                if (task.isDone()) {
+                    callable.call();
+                } else {
+                    CompletableFuture.runAsync(this);
+                }
+            } catch (Exception ignored) {
             }
-
-            return null;
-        }
-    }
-
-    private static void invokeIgnoringExceptions(Callable<?> callable) {
-        try {
-            callable.call();
-        } catch (Exception ignored) {
         }
     }
 }
